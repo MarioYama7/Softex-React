adf6410b9c31be0f52bccf2aa6c957e132205980
MarioYama7 <mariohenrique.777@gmail.com>
2022-10-12 10:48:44 -0300

    Projeto 03

---
+++ PROJETO 03/src/App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+/*import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Mário Henrique
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;*/
+
+
+
+  function App(){ // Nova função para aparecer um texto simples na página
+
+    return (
+    <div>
+      <h1>Olá Mundo!</h1>
+      <p>Teste</p>
+    </div>
+    )
+  }
+
+  export default App;
---
+++ PROJETO 03/src/Topo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Topo () {
+
+    return (
+        <div>
+            <h2>Topo do site</h2>
+        </div>
+    );
+}
+
+export default Topo;
